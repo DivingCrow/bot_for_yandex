ede0d85a0f8234b5f85acb7b8748ad53767904d5
DivingCrow <67337171+DivingCrow@users.noreply.github.com>
2020-07-09 21:14:37 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
+// ==UserScript==
+// @name         bot_for_google
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://yandex.ru/*
+// @grant        none
+// ==/UserScript==
 
+console.log("Hello!");
